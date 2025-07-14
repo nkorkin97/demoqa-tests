c7360de3c45517fb2902ed5fd0f037198b7b9ced
tearsOf-Autumn <korkinnikita2208@gmail.com>
2025-07-14 14:55:57 +0300

    fixed dependencies

--- src/test/java/FirstJUnitTest.java
+++ src/test/java/FirstJUnitTest.java
@@ -1,12 +1,37 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class FirstJUnitTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("это метод @BeforeAll");
+    }
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("это метод @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("это метод @AfterEach");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("это метод @AfterAll");
+    }
+
     @Test
     public void firstTest() {
+        System.out.println("это первый тест");
         Assertions.assertTrue(true);
     }
+
+    @Test
+    void secondeTest() {
+        System.out.println("Это второй тест");
+    }
+
 }
