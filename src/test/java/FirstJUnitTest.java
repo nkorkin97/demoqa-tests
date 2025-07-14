19e4ce4f8fc5a86a6f155465e5ce796acfbb886b
tearsOf-Autumn <korkinnikita2208@gmail.com>
2025-07-14 14:25:35 +0300

    added JUnit and Selenide

--- src/test/java/FirstJUnitTest.java
+++ src/test/java/FirstJUnitTest.java
@@ -1,7 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class FirstJUnitTest {
 
     @Test
-
+    public void test() {
+        open();
+    }
 }
