014f23ce845c05f91a55383e004605b53083374a
tearsOf-Autumn <korkinnikita2208@gmail.com>
2025-07-30 14:52:49 +0300

    init gh-pages

---
+++ src/test/java/lessons/fifteenth/allure/WebSteps.java
@@ -0,0 +1,48 @@
+package lessons.fifteenth.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.commands.TakeScreenshot;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com/");
+    }
+
+    @Step("Ищем репозиторий")
+    public void searchForRepo(String repository) {
+        $(".search-input").scrollTo().click();
+        $("#query-builder-test").scrollTo().setValue(repository);
+        $("#query-builder-test").scrollTo().submit();
+    }
+
+    @Step("Кликаем по ссылке на репозиторий")
+    public void clickOnRepo() {
+        $("[data-testid = 'results-list'] [href='/eroshenkoam/allure-example']").scrollTo().click();
+    }
+
+    @Step("Открываем вкладку пулл реквестов")
+    public void openPullRequestTab() {
+        $("#pull-requests-tab").scrollTo().click();
+    }
+
+    @Step("Проверяем issue")
+    public void shoulSeeRequestWithNumber() {
+        $(".js-navigation-container").$("#issue_79").shouldBe(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
---
+++ src/test/java/simple/PositiveTests.java
@@ -0,0 +1,46 @@
+package simple;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Simple")
+public class PositiveTests {
+
+    @Test
+    void test1() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test2() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test3() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test4() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test5() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test6() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test7() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test8() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test9() {
+        Assertions.assertTrue(true);
+    }
+}
